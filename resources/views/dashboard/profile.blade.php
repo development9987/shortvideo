ca688dc27c7387f65908578cf85e947471ba73aa
haseebdivinatorysolutions <haseeb@divinatorysolutions.com>
2022-04-27 15:42:36 +0500

    profile page changes

--- resources/views/dashboard/profile.blade.php
+++ resources/views/dashboard/profile.blade.php
@@ -96,7 +96,7 @@
                            <div class="mr-5"><b>{{$followers}}</b> Followers</div>
                         </div>
                         <a class="card-footer text-white clearfix small z-1" href="#">
-                        <span class="float-left">View Details</span>
+                        <!-- <span class="float-left">View Details</span> -->
                         <span class="float-right">
                         <i class="fas fa-angle-right"></i>
                         </span>
@@ -112,7 +112,7 @@
                            <div class="mr-5"><b>{{$following}}</b> Followings</div>
                         </div>
                         <a class="card-footer text-white clearfix small z-1" href="#">
-                        <span class="float-left">View Details</span>
+                        <!-- <span class="float-left">View Details</span> -->
                         <span class="float-right">
                         <i class="fas fa-angle-right"></i>
                         </span>
